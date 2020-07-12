4be8b3d25e47255442ddef6172ce971708b34588
t-matsumo <tatsukimatsumo@gmail.com>
2020-07-12 12:45:08 +0900

    aa

--- src/main/kotlin/library/Others.kt
+++ src/main/kotlin/library/Others.kt
@@ -1,10 +1,5 @@
 package library
 
-fun calcCumulativeSumList(list: List<Int>) = list.fold(mutableListOf(0)) { acc, value ->
-    acc += acc.last() + value
-    return acc
-}
-
 fun sumIntFromOneTo(last: Int) = last * (last + 1) / 2
 
 fun calcMaxIndex(list: List<Int>) = list.indices.maxBy { list[it] }!!
