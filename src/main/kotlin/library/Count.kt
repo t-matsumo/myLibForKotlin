4415f1d973a8dfc4b7dd2e9f5e7707693942737e
t-matsumo <tatsukimatsumo@gmail.com>
2020-11-28 11:59:22 +0900

    aa

--- src/main/kotlin/library/Count.kt
+++ src/main/kotlin/library/Count.kt
@@ -3,6 +3,7 @@ package library
 /** O(n) */
 fun <T> elementCountMap(list: List<T>) = list.groupingBy { it }.eachCount()
 
+// 二分法を使うとloglog(n)でできるらしい？
 /** O(log(n)) */
 tailrec fun countDiv(n: Long, divisor: Long, count: Long = 0L): Long =
     if (n % divisor == 0L) countDiv(n / divisor, divisor, count + 1) else count
