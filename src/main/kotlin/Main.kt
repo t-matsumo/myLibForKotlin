39ae85c34db3da924a100f04b8b1f4121db500b7
t-matsumo <tatsukimatsumo@gmail.com>
2021-12-04 23:27:26 +0900

    aaaa

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,23 @@
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import java.io.PrintWriter
 import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
-val br = BufferedReader(InputStreamReader(System.`in`))
-val out = PrintWriter(System.out)
+val br = System.`in`.bufferedReader()
+val out = PrintWriter(System.out.bufferedWriter())
 
 fun main() {
-    br.use {
-        out.use {
-//            repeat(readInteger()) { solve() }
-            solve()
-            out.flush()
-        }
-    }
+//    repeat(readInt()) { solve() }
+    solve()
+    out.flush()
 }
 
 fun solve() {
 }
 
 fun readLine() = br.readLine()!!
-fun readInteger() = Integer.parseInt(br.readLine(), 10)
-fun readLong() = java.lang.Long.parseLong(br.readLine(), 10)
-fun readStringList() = br.readLine()!!.split(" ")
-fun readIntegerList() = br.readLine()!!.split(" ").map { Integer.parseInt(it, 10) }
-fun readLongList() = br.readLine()!!.split(" ").map { java.lang.Long.parseLong(it, 10) }
-fun readDoubleList() = br.readLine()!!.split(" ").map { java.lang.Double.parseDouble(it) }
+fun readInt() = readLine().toInt()
+fun readLong() = readLine().toLong()
+fun readStringList() = readLine().split(' ')
+fun readIntList() = readStringList().map(String::toInt)
+fun readLongList() = readStringList().map(String::toLong)
