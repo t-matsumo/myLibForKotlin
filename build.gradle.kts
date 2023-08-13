0b1cb7dbe4b4660ee481e34f1418975c1b0a20b5
Matsumoto Tatsuki <tatsukimatsumo@gmail.com>
2023-08-13 23:41:22 +0900

    set Java version 19

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,13 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+//    implementation(kotlin("stdlib-jdk8"))
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
+}
+
+kotlin {
+    jvmToolchain(19)
 }
 
 tasks {
